d1450fde9f039ce65b4bfac3e832319ff5ef9fad
yllee9127 <yllee9127@gmail.com>
2025-02-04 21:25:21 +0800

    Adding main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "static_bucket" {
+ bucket = "yls3.sctp-sandbox.com"
+ force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "enable_public_access" {
+  bucket = aws_s3_bucket.static_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.static_bucket.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.static_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
